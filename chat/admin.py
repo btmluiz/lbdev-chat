2424fbc7993d65a23c99a3489666849e2de90a3a
Luiz Braga <luiz.braga@faciltecnologia.com.br>
2021-10-10 18:49:41 -0300

    add models to django admin

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from chat import models
+
+admin.site.register(models.Chat)
+admin.site.register(models.ChatSession)
+admin.site.register(models.ChatHistory)
